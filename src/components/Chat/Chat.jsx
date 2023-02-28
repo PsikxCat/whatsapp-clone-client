6ea67c343c44c63ecda866e97bb373882ad9217a
PsikxCat <arevalogrichard@gmail.com>
2023-02-28 13:42:26 -0500

    🎨 Componente Chat

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { MdTagFaces, MdMic, MdAttachFile } from 'react-icons/md';
 
+import Message from './Message/Message';
 import './Chat.scss';
 
 function Chat() {
   return (
-    <div className='app__chat'>Chat</div>
+    <section className='app__chat'>
+      <div className='chat__header'>
+        <img src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/00ae680e42c34a91c88bd4594c1c4149111c6293_full.jpg" alt="profile picture" />
+
+        <div className="header__info">
+          <h3 className='info__name'>
+            Heisenberg
+          </h3>
+          <p className='info__status'>
+            Online
+          </p>
+        </div>
+      </div>
+
+      <div className='chat__body'>
+        <Message
+          isSentByMe={true}
+          message='Hola, ¿cómo estás?'
+        />
+        <Message
+          isSentByMe={false}
+          message='Bien, ¿y tú?'
+        />
+      </div>
+
+      <div className='chat__footer'>
+        <MdTagFaces className='container__icon' />
+        <MdAttachFile className='container__icon' />
+
+        <div className='footer__container'>
+          <input type="text" placeholder='Escribe un mensaje aquí' />
+        </div>
+
+        <MdMic className='container__icon' />
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/Chat/Message/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Message.scss';
+
+function Message({ isSentByMe, message }) {
+  return (
+  <div className={'chat__div ' + (isSentByMe ? 'sent-by-me' : 'sent-by-other')}>
+    <p className={'chat__message ' + (isSentByMe ? 'sent-by-me' : 'sent-by-other')}>
+      { message }
+      <span className='message__timestamp'>
+        {new Date().toUTCString()}
+      </span>
+    </p>
+  </div>
+  );
+};
+
+Message.propTypes = {
+  isSentByMe: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default Message;
